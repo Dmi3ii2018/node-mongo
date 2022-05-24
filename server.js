833ea700657f666c42ac87c16eb8e3c0321ee36c
Dmitrii Alekseev <dmitry.alekseev@flatstack.com>
2022-05-24 11:53:49 +0500

    add PUT, DELETE

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -10,7 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
-const db = 'mongodb+srv://dmitry:9403@cluster0.p4vod.mongodb.net/node-blog?retryWrites=true&w=majority';
+const db = 'mongodb+srv://dmitry:9403@cluster0.p4vod.mongodb.net/node-blog?retryWrites=true&w=majority'
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,11 +24,13 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-
 app.use(express.urlencoded({ extended: false }));
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -42,33 +45,70 @@ app.get('/contacts', (req, res) => {
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
-    })
+    });
 });
 
 app.get('/posts/:id', (req, res) => {
   const title = 'Post';
-  const post = {
-    id: '1', 
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-    title: 'Post title',
-    date: '05.05.2021',
-    author: 'Yauhen',
-  };
-  res.render(createPath('post'), { title, post });
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
 });
 
 app.get('/posts', (req, res) => {
   const title = 'Posts';
-  const posts = [
-    {
-      id: '1', 
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-      title: 'Post title',
-      date: '05.05.2021',
-      author: 'Yauhen',
-    }
-  ];
-  res.render(createPath('posts'), { title, posts });
+  Post
+    .find()
+    .sort({ createdAt: -1 })
+    .then(posts => res.render(createPath('posts'), { posts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), { title });
 });
 
 app.post('/add-post', (req, res) => {
@@ -76,16 +116,11 @@ app.post('/add-post', (req, res) => {
   const post = new Post({ title, author, text });
   post
     .save()
-    .then((result) => res.send(result))
+    .then((result) => res.redirect('/posts'))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
-    })
-});
-
-app.get('/add-post', (req, res) => {
-  const title = 'Add Post';
-  res.render(createPath('add-post'), { title });
+    });
 });
 
 app.use((req, res) => {
