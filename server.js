43a8d3a0a14de3dee9503cfa3bf9a880bdbf515c
Dmitrii Alekseev <dmitry.alekseev@flatstack.com>
2022-05-26 10:57:02 +0500

    add env

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const chalk = require('chalk');
+require('dotenv').config();
 const methodOverride = require('method-override');
-const Post = require('./models/post');
-const Contact = require('./models/contact');
+const PostRoutes = require('./routes/post-routes')
+const ContactRoutes = require('./routes/contact-routes')
+const createPath = require('./helpers/create-path')
+
+const errMsg = chalk.bgKeyword('white').redBright;
+const succMsg = chalk.bgKeyword('green').white;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
-const db = 'mongodb+srv://dmitry:9403@cluster0.p4vod.mongodb.net/node-blog?retryWrites=true&w=majority'
-
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((res) => console.log('Connected to DB'))
-  .catch((error) => console.log(error));
+  .connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log(succMsg('Connected to DB')))
+  .catch((error) => console.log(errMsg(error)));
 
-const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
+app.listen(process.env.PORT, (error) => {
+  error ? console.log(errMsg(error)) : console.log(succMsg(`listening port ${process.env.PORT}`));
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -37,91 +38,8 @@ app.get('/', (req, res) => {
   res.render(createPath('index'), { title });
 });
 
-app.get('/contacts', (req, res) => {
-  const title = 'Contacts';
-  Contact
-    .find()
-    .then(contacts => res.render(createPath('contacts'), { contacts, title }))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.get('/posts/:id', (req, res) => {
-  const title = 'Post';
-  Post
-    .findById(req.params.id)
-    .then(post => res.render(createPath('post'), { post, title }))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.delete('/posts/:id', (req, res) => {
-  Post
-    .findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.get('/edit/:id', (req, res) => {
-  const title = 'Edit Post';
-  Post
-    .findById(req.params.id)
-    .then(post => res.render(createPath('edit-post'), { post, title }))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.put('/edit/:id', (req, res) => {
-  const { title, author, text } = req.body;
-  const { id } = req.params;
-  Post
-    .findByIdAndUpdate(id, { title, author, text })
-    .then((result) => res.redirect(`/posts/${id}`))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.get('/posts', (req, res) => {
-  const title = 'Posts';
-  Post
-    .find()
-    .sort({ createdAt: -1 })
-    .then(posts => res.render(createPath('posts'), { posts, title }))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
-
-app.get('/add-post', (req, res) => {
-  const title = 'Add Post';
-  res.render(createPath('add-post'), { title });
-});
-
-app.post('/add-post', (req, res) => {
-  const { title, author, text } = req.body;
-  const post = new Post({ title, author, text });
-  post
-    .save()
-    .then((result) => res.redirect('/posts'))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), { title: 'Error' });
-    });
-});
+app.use(PostRoutes)
+app.use(ContactRoutes)
 
 app.use((req, res) => {
   const title = 'Error Page';
